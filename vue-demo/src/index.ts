be7ac9acf427c74eb1a14d5f4a7aa3f7531f674a
ShenQiuhui <pandashen821@163.com>
2021-06-23 01:05:29 +0800

    feat: vue-demo complate

--- vue-demo/src/index.ts
+++ vue-demo/src/index.ts
@@ -1,10 +1,18 @@
 import Vue from 'vue';
-// import Hello from './components/Hello.vue'
+import Hello from './components/Hello.vue'
+
+// new Vue({
+//   el: '.app',
+//   data: {
+//     name: 'TypeScript'
+//   },
+//   template: '<h1>Hello {{ name }}</h1>'
+// });
 
 new Vue({
   el: '.app',
-  data: {
-    name: 'TypeScript'
+  components: {
+    Hello
   },
-  template: '<h1>Hello {{ name }}</h1>'
+  template: '<hello />'
 });
